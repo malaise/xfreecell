52534feeac6cf37733b417ab36e71397524cc0d8
Pascal MALAISE <malaise@magic.fr>
2012-04-04 20:26:03 +0200

    Update release

--- general.h
+++ general.h
@@ -4,7 +4,7 @@
 #include <errno.h>
 
 static const char DefaultBackground[] = "forestgreen";
-static const char VersionStr[] = "xfreecell 2.7";
+static const char VersionStr[] = "xfreecell 2.8";
 
 const int mainWindowWidth = 670, mainWindowHeight = 700;
 
